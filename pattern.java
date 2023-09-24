c63e6f869fecaa5b06e13956cc08eb4151b8e76c
sahiljarngal <97387822+sahiljarngal@users.noreply.github.com>
2023-09-24 22:39:23 +0530

    pattern Questions

---
+++ pattern.java
@@ -0,0 +1,546 @@
+import java.util.*;    /*IMPORT THIS JAVA LIBRARY IS IMPORTANT TO WRITE BEFORE YOU WRITE YOUR CODE */
+
+// -----PATTERN-1------|
+
+// class pattern {
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("no. of rows: ");
+        //    int n=input.nextInt();
+        //    System.out.print("no. of columns: ");
+        //    int m=input.nextInt();
+        //    for (int i=1;i<=m;i++){
+                // for(int j=1;j<=n;j++){
+                        // System.out.print("*");
+                // }
+                // System.out.println("");
+        //    }
+        //    input.close();
+        // }
+// }
+// 
+
+
+// ----------PATTERN-2-------|
+
+
+// 
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input =new Scanner(System.in);
+                //  System.out.print("no. of rows: ");
+                // int n=input.nextInt();
+                //      System.out.print("no. of columns: ");
+                // int m=input.nextInt();
+                // for(int i=1;i<=n;i++){
+                        // for(int j=1;j<=m;j++){
+                                // if(i==1||j==1||i==n||j==m){
+                                        // System.out.print("*");
+                                // }
+                                // else{
+                                        // System.out.print(" ");
+                                // }
+                        // }
+                        // System.out.println("");
+                // }
+                // input.close();
+        // }
+// }
+// 
+// 
+
+
+// --------PATTERN-3----|
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                //  System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+        //        
+                // for(int i=1;i<=n;i++){
+                        // for(int j=1;j<=i;j++){
+                                // System.out.print("*");
+                        // }
+                        // System.out.println("");
+                // }
+        // }
+// }
+// 
+
+
+
+// ---------PATTERN-4------|
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // for(int i=1;i<=n;i++){
+                        // for(int j=i;j<=n;j++){
+                                // System.out.print("*");
+                        // }
+                        // System.out.println("");
+                // }
+        // }
+// }
+// 
+
+
+
+// -------PATTERN-5---------|
+
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // for(int i=n;i>=1;i--){
+                        // for(int j=1;j<i;j++){
+                                // System.out.print(" ");
+                        // }
+                        // for(int j=0;j<=n-i;j++){
+                        // System.out.print("*");
+                        //    }
+                        // System.out.println("");
+                // }
+        // }
+// }
+// 
+
+
+
+// -------PATTERN-6---------|
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // for(int j=1;j<=n;j++){
+                        // for(int i=1;i<=j;i++){
+                                // System.out.print(i +" ");
+                        // }
+                        // System.out.println("");
+                // }
+        // }
+// }
+
+
+
+
+// -------PATTERN-7-----|
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // for(int i=0;i<n;i++){
+                        // for(int j=1;j<=n-i;j++){
+                                // System.out.print(j);
+                        // }
+                        // System.out.println("");
+                // }
+        // }
+// }
+// 
+
+
+// ------PATTERN-8-------|
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // int a=0;    /*a= the count of numbers */
+                // for(int i=1;i<=n;i++){
+                        // for(int j=1;j<=i;j++){
+                                // a++;
+                                // System.out.print(a + " ");
+                        // }
+                        // System.out.println("");
+                // }
+        // }
+// }
+
+
+
+// ----------PATTERN-9-------|
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // for(int i=1;i<=n;i++){
+                        // int a=1;
+                        // for(int j=1;j<=i;j++){
+                                // if((i+j)%2==0){
+                                // System.out.print(1 + " ");
+                                // }
+                                // else{
+                                        // System.out.print(0 + " ");
+                                // }
+                        // }
+                        // System.out.println("");
+                // }
+        // }
+// }
+
+
+
+
+// -----------PATTERN-10------------|
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // for(int i=0;i<n;i++){
+                        // for(int j=1;j<=n-i;j++){
+                                // System.out.print(" ");
+// 
+                        // }    
+                // for(int j=1;j<=n;j++){
+                        // System.out.print("*");
+                // }
+                // System.out.println("");            
+        // }
+// 
+        // }
+// }
+
+
+
+
+// ---------PATTERN-11---------|
+
+
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // for(int i=0;i<=n;i++){
+                        // for(int j=1;j<=n-i;j++){
+                                // System.out.print(" ");
+                                // 
+                        // }
+                        //  for(int j=1;j<=i;j++){
+                                // System.out.print(i +" ");
+                        //  }
+                        //  System.out.println("");
+                // }
+        // }
+// }
+
+
+
+
+// ---------PATTERN-12--------|
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // for(int i=0;i<n;i++){
+                        // for(int j=1;j<=n-i;j++){
+                                // System.out.print(" ");
+                        // }
+                        // for(int j=i+1;j>=1;j--){
+                                // System.out.print(j);
+                        // }
+                        // if(i>=1){
+                        // for(int j=2;j<=i+1;j++){
+                                // System.out.print(j);
+                        // }
+                // }
+                        // System.out.println("");
+                // }
+        // }
+// }
+
+
+
+
+
+// ---------PATTERN-13--------|
+
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // for(int i=1;i<=n;i++){
+                        // for(int j=1;j<=i;j++){
+                                // System.out.print("*");}
+                        //      for(int j=1;j<=n-i;j++){
+                                // System.out.print(" ");
+                        //      }
+                        //      for(int k=1;k<=n-i;k++){
+                                // System.out.print(" ");
+                        //      }
+                        //      for(int l=1;l<=i;l++){
+                                // System.out.print("*");
+                        //      }
+// 
+                        //      System.out.println("");
+                        // }
+                        // for(int b=1;b<=n;b++){
+                                // for(int j=1;j<=n-b;j++){
+                                        // System.out.print("*");
+                                // }
+                                // for(int j=1;j<=b;j++){
+                                        // System.out.print(" ");
+                                // }
+                                // for(int j=1;j<=b;j++){
+                                //  System.out.print(" ");
+                                // }
+                                // for(int j=1;j<=n-b;j++){
+                                        // System.out.print("*");
+                                // }
+                                // System.out.println("");
+                        // }
+                        // input.close();
+                // }
+        // }
+
+
+
+
+
+        // ------PATTERN-14---|
+
+
+
+
+
+//         class pattern{
+//                 public static void main(String[] args) {
+//                         Scanner input=new Scanner(System.in);
+//                         System.out.print("Enter the length: ");
+//                         int n=input.nextInt();
+//                         for(int i=1;i<=n;i++){
+//                                 for(int j=1;j<=n-i;j++){
+//                                         System.out.print(" ");
+//                                 }
+                                 
+                                 
+                                 
+//                                 for(int j=1;j<=i;j++){
+//                                         System.out.print("*");
+//                                 }
+                              
+//                                for(int j=2;j<=i;j++){
+//                                  System.out.print("*");
+//                                  }
+                              
+//                                 System.out.println("");
+//                         }
+                       
+//                        for(int i=0;i<n;i++){
+//                          for(int j=1;j<=i;j++){
+//                       System.out.print(" ");
+//                        }
+//                         for(int j=1;j<=n-i;j++){
+//                                 System.out.print("*");
+//                         }
+//                        for(int j=1;j<n-i;j++){
+//                         System.out.print("*");
+//                        }
+                       
+                       
+//                         System.out.println("");
+//                        }
+//         }
+// }
+// 
+
+
+
+
+// -----------PATTERN-15----------|
+
+
+
+
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // for(int i=1;i<=n;i++){
+                        // for(int j=1;j<=i;j++){
+                                // if(j==1 || j==i){
+                                // System.out.print("*");}
+                                // else{
+                                //  System.out.print(" ");
+                                // }
+                        //       }
+                        //  for(int j=1;j<=n-i;j++){
+                                //     System.out.print(" ");
+                        //     }
+                        //     for(int j=1;j<=n-i;j++){
+                                // System.out.print(" ");
+// 
+                        //     }
+                        //     for(int j=1;j<=i;j++){
+                        //     if(j==1 ||j==i){
+                                // System.out.print("*");}
+                        //    else {
+                                // System.out.print(" ");
+                        //    }
+                        //       
+                        // }
+                        // 
+                        //     System.out.println("");
+                // }
+                // for(int i=0;i<n;i++){
+                        // for(int j=1;j<=n-i;j++){
+                                // if(j==1||j==n-i){
+                                        // System.out.print("*");}
+                                        // else{
+                                                // System.out.print(" ");
+                                        // }
+                                // }
+                                        // for(int j=1;j<=i;j++){
+                                                // System.out.print(" ");
+                                        // }
+                                        // for(int j=1;j<=n;j++){
+                                                // if(j==i+1 ||j==n){
+                                                // System.out.print("*");
+                                                // }
+                                                // else{
+                                                        // System.out.print(" ");
+                                                // }
+                                        // }
+                                        // System.out.println("");
+                        //        
+                        // }
+                // }
+// }
+
+
+
+
+
+
+
+
+
+// -----------------PATTERN-16--------------|
+
+
+
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // for(int i=1;i<=n;i++){
+                        // if(i==1 ||i==n){
+                                // for(int j=1;j<=n;j++){
+                                        // System.out.print("*");
+                                // }
+                                // 
+                        // }
+                //    if(i>1 && i<n){
+                        // for(int j=1;j<=n;j++){
+                        //  if(j==1 || j==n){
+                                // System.out.print("*");
+                        //  }
+                        //  else{
+                                // System.out.print(" ");
+                        //  }
+                        // }
+                // }
+                        // System.out.println(" ");
+                // }
+        // }
+// }
+// 
+
+
+
+// ----------PATTERN--17-----------|
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // for(int i=0;i<n;i++){
+                        // for(int j=1;j<=n-i;j++){
+                                // System.out.print(" ");
+                        // }
+                        // for(int j=0;j<=i;j++){
+                                // System.out.print(j+1 +" ");
+                        // }
+                        // System.out.println("");
+                // }
+        // }
+// }
+
+
+
+
+// -----------PATTERN-18-----------|
+
+
+
+
+
+// class pattern{
+        // public static void main(String[] args) {
+                // Scanner input=new Scanner(System.in);
+                // System.out.print("Enter the length: ");
+                // int n=input.nextInt();
+                // for(int i=0;i<n;i++){
+                        // for(int j=1;j<=n-(n-i);j++){
+                                // System.out.print(" ");
+                        // }
+                        // for(int j=1;j<=n-i;j++){
+                                // System.out.print(i+1 +" ");
+                        // }
+                        // System.out.println("");
+                // }
+        // }
+// }
